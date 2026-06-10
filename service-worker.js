dd3d9e5f8104d37a6a7514c59c40847920f56512
vincearion <mr.vjaonl@gmail.com>
2026-06-10 10:33:48 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'hogfarm-v10';
+var urlsToCache = [
+  '/hog-farm-tracker/',
+  '/hog-farm-tracker/index.html',
+  '/hog-farm-tracker/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
